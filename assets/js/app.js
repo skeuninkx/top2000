fe89889fe6f8d4663b3bfdf6cd17c14f497843f0
Sjors Keuninkx <sjors.keuninkx@gmail.com>
2019-10-01 20:33:37 +0200

    Fixed issues with running foundation

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,26 +7,16 @@
 
 // any CSS you require will output into a single css file (app.css in this case)
 require('../css/app.scss');
-require('jquery-ui/themes/base/core.css');
-require('jquery-ui/themes/base/resizable.css');
-require('jquery-ui/themes/base/selectable.css');
-require('jquery-ui/themes/base/accordion.css');
-require('jquery-ui/themes/base/autocomplete.css');
-require('jquery-ui/themes/base/button.css');
-require('jquery-ui/themes/base/datepicker.css');
-require('jquery-ui/themes/base/dialog.css');
-require('jquery-ui/themes/base/menu.css');
-require('jquery-ui/themes/base/progressbar.css');
-require('jquery-ui/themes/base/slider.css');
-require('jquery-ui/themes/base/spinner.css');
-require('jquery-ui/themes/base/tabs.css');
-require('jquery-ui/themes/base/tooltip.css');
-require('jquery-ui/themes/base/theme.css');
+require('jquery-ui/themes/base/all.css')
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // const $ = require('jquery');
+var $ = require('jquery');
 
-require('jquery');
 require('jquery-ui/ui/core');
 require('foundation-sites');
 require('foundation-sites/js/foundation');
+
+$(function() {
+    $(document).foundation();
+})
