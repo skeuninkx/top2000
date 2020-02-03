12c7fb6cf18ea8f2d725318409c1270a4df0fe41
Sjors Keuninkx <sjors.keuninkx@gmail.com>
2020-02-03 18:57:47 +0100

    Add new command to import top 2000 editions separatly

--- src/Command/ImportDataCommand.php
+++ src/Command/ImportDataCommand.php
@@ -8,51 +8,47 @@ use App\Entity\Artist;
 use App\Entity\Edition;
 use App\Entity\Position;
 use App\Entity\Song;
-use Doctrine\Common\Persistence\ObjectManager;
-use Doctrine\ORM\OptimisticLockException;
-use Doctrine\ORM\ORMException;
-use PhpOffice\PhpSpreadsheet\Reader\Csv;
-use PhpOffice\PhpSpreadsheet\Reader\Exception;
+use Doctrine\ORM\EntityManagerInterface;
+use PhpOffice\PhpSpreadsheet;
+use PhpOffice\PhpSpreadsheet\Reader;
+use PhpOffice\PhpSpreadsheet\Reader\Xlsx;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Filesystem\Exception\FileNotFoundException;
+use Symfony\Component\Console\Question\ChoiceQuestion;
+use Symfony\Component\Finder\Finder;
 use Symfony\Component\HttpKernel\KernelInterface;
 
 /**
  * Class ImportDataCommand
+ *
+ * @author Sjors Keuninkx <sjors.keuninkx@gmail.com>
  */
 class ImportDataCommand extends Command
 {
-    /**
-     * @var ObjectManager
-     */
-    private $om;
-
     /**
      * @var KernelInterface
      */
     private $kernel;
 
     /**
-     * @var Edition[]
+     * @var EntityManagerInterface
      */
-    private $editions;
+    private $em;
 
     /**
      * Constructor
      *
-     * @param ObjectManager $om
      * @param KernelInterface $kernel
      */
-    public function __construct(ObjectManager $om, KernelInterface $kernel)
+    public function __construct(KernelInterface $kernel, EntityManagerInterface $em)
     {
         parent::__construct('app:import:data');
 
-        $this->om = $om;
         $this->kernel = $kernel;
+        $this->em = $em;
     }
 
     /**
@@ -60,10 +56,10 @@ class ImportDataCommand extends Command
      */
     protected function configure()
     {
-        $this->setName('app:import:data')
-            ->setDescription('Import all Top 2000 data')
-            ->setHelp('Import all Top 2000 data')
-            ->addArgument('file', InputArgument::REQUIRED, 'Name the file, located in the import-folder');
+        $this->setDescription('Import Top 2000 data')
+            ->setHelp('Import Top 2000 data')
+            ->addOption('all', 'a', InputOption::VALUE_NONE, 'Import all existing years')
+        ;
     }
 
     /**
@@ -71,131 +67,102 @@ class ImportDataCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        ini_set('memory_limit', '256M');
+        $helper = $this->getHelper('question');
 
-        $path = sprintf('%s/%s', $this->kernel->getProjectDir() . '/import', $input->getArgument('file'));
-
-        if (file_exists($path) && ($spreadsheet = $this->getSpreadsheet($path))) {
-            $editionsCreated = false;
-            $counter = 0;
-
-            try {
-                $worksheet = $spreadsheet->setActiveSheetIndex(0);
-            } catch (\Exception $e) {
-                $output->writeln('Worksheet could not be loaded!');
-                exit;
-            }
-
-            foreach ($worksheet->toArray() as $key => $row) {
-                if ($key === 0 && !$editionsCreated) {
-                    $this->setupEditions($row);
-                    $editionsCreated = true;
-                    continue;
-                }
-
-                $row = array_map('trim', $row);
-
-                if ($song = $this->setupSong($row)) {
-                    ++$counter;
-
-                    if (($counter % 100) === 0) {
-                        $this->om->flush();
-                        $this->om->clear();
-
-                        $counter = 0;
-                    }
-                } else {
-                    $output->writeln(sprintf('Something went wrong during import %s - %s', $row[0], $row[1]));
-                }
+        if ($input->getOption('all')) {
+            foreach ($this->getYears() as $year) {
+                $this->importYear(intval($year));
             }
         } else {
-            throw new FileNotFoundException(null, 0, null, $path);
+            $question = new ChoiceQuestion('Which year needs to be imported?', $this->getYears());
+            $answer = $helper->ask($input, $output, $question);
+
+            $this->importYear(intval($answer));
+        }
+    }
+
+    /**
+     * @return array
+     */
+    private function getYears(): array
+    {
+        $years = [];
+
+        $finder = new Finder();
+        $finder->files()->in(sprintf('%s/%s', $this->kernel->getProjectDir(), 'import'));
+
+        foreach ($finder as $file) {
+            if (preg_match('/^TOP-2000-(\d{4}).xlsx$/i', $file->getFilename(), $matches)) {
+                $years[] = $matches[1];
+            }
         }
 
-        // Flush entities, which will not be added to batch
-        $this->om->flush();
-        $this->om->clear();
+        $years = array_unique($years);
+        sort($years);
+
+        return $years;
+    }
+
+    /**
+     * @param int $year
+     * @return bool
+     */
+    private function importYear(int $year): bool
+    {
+        $path = sprintf('%s/%s/%s', $this->kernel->getProjectDir(), 'import', sprintf('TOP-2000-%d.xlsx', $year));
+
+        if ($spreadSheet = $this->getSpreadSheet($path)) {
+            try {
+                $first = true;
+                $edition = null;
+
+                $sheet = $spreadSheet->setActiveSheetIndex(0);
+
+                foreach ($sheet->toArray() as $item) {
+                    if ($first === true) {
+                        $edition = (new Edition())
+                            ->setYear(intval($item[0]))
+                            ->setDescription(sprintf('Dit is de Top 2000 van %d', $item[0]));
+                        $this->em->persist($edition);
+
+                        $first = false;
+
+                        continue;
+                    }
+
+                    $artist = $this->getArtist($item[2]);
+                    $song = $this->getSong($artist, $item[1], intval($item[3]));
+
+                    // Setup position
+                    $position = (new Position())
+                        ->setEdition($edition)
+                        ->setSong($song)
+                        ->setNumber(intval($item[0]));
+
+                    $this->em->persist($position);
+                    $this->em->flush();
+                }
+
+                return true;
+            } catch (PhpSpreadsheet\Exception $e) {
+                return false;
+            }
+        }
     }
 
     /**
      * @param string $path
-     * @return Spreadsheet|null
+     * @return Spreadsheet
      */
-    private function getSpreadsheet(string $path)
+    private function getSpreadSheet(string $path): Spreadsheet
     {
         try {
-            $reader = (new Csv())
-                ->setInputEncoding('UTF-8')
-                ->setDelimiter(';')
-                ->setEnclosure(0)
-                ->setSheetIndex(0)
+            $reader = (new Xlsx())
                 ->setReadDataOnly(true);
 
             return $reader->load($path);
-        } catch (Exception $e) {
+        } catch (Reader\Exception $e) {
             return null;
-        }
-    }
-
-    /**
-     * @param array $row
-     * @return bool
-     */
-    private function setupEditions(array $row): bool
-    {
-        for ($i = 4; $i < count($row); $i++) {
-            $year = intval($row[$i]);
-
-            if ($year > 0) {
-                if (!$edition = $this->om->getRepository(Edition::class)->findOneByYear($year)) {
-                    $edition = (new Edition())
-                        ->setYear(intval($year))
-                        ->setDescription(sprintf('Dit is de Top 2000 uit het jaar %d', $year));
-                    $this->om->persist($edition);
-                }
-
-                // Setup cached list of all available editions
-                $this->editions[$i] = $edition;
-            }
-        }
-
-        try {
-            $this->om->flush();
-
-            return true;
-        } catch (\Exception $e) {
-            return false;
-        }
-    }
-
-    /**
-     * @param array $row
-     * @return Song|bool
-     */
-    private function setupSong(array $row)
-    {
-        $artist = $this->getArtist($row[0]);
-        $song = $this->getSong($artist, $row[1], intval($row[2]));
-
-        for ($i = 4; $i < count($row); $i++) {
-            $number = intval($row[$i]);
-
-            if ($number <= 2000 && array_key_exists($i, $this->editions)) {
-                $position = (new Position())
-                    ->setEdition($this->editions[$i])
-                    ->setSong($song)
-                    ->setNumber($number);
-
-                $song->addPosition($position);
-            }
-        }
-
-        try {
-            $this->om->persist($song);
-
-            return $song;
-        } catch (\Exception $e) {
-            return false;
         }
     }
 
@@ -205,13 +172,9 @@ class ImportDataCommand extends Command
      */
     private function getArtist(string $name): Artist
     {
-        if (!$artist = $this->om->getRepository(Artist::class)->findOneByName($name)) {
+        if (!$artist = $this->em->getRepository(Artist::class)->findOneBy(['name' => $name])) {
             $artist = (new Artist())
                 ->setName($name);
-
-            // Save artist data always, because we won't get duplicate items
-            $this->om->persist($artist);
-            $this->om->flush($artist);
         }
 
         return $artist;
@@ -219,17 +182,17 @@ class ImportDataCommand extends Command
 
     /**
      * @param Artist $artist
-     * @param string $title
-     * @param int $releaseYear
+     * @param string $name
+     * @param int $released
      * @return Song
      */
-    private function getSong(Artist $artist, string $title, int $releaseYear): Song
+    private function getSong(Artist $artist, string $name, int $released): Song
     {
-        if (!$song = $this->om->getRepository(Song::class)->findOneBy(['artist' => $artist, 'name' => $title])) {
+        if (!$song = $this->em->getRepository(Song::class)->findOneBy(['name' => $name, 'artist' => $artist])) {
             $song = (new Song())
-                ->setName($title)
-                ->setArtist($artist)
-                ->setReleased($releaseYear);
+                ->setName($name)
+                ->setReleased($released)
+                ->setArtist($artist);
         }
 
         return $song;
